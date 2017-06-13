f4e3d419b82d711502a3f67dafcfb8944ae2c769
Jack Bittiner <jackbittiner@hotmail.com>
2017-06-13 10:10:10 +0100

    Added DJ data

---
+++ src/data/djs.js
@@ -0,0 +1,35 @@
+'use strict';
+
+const djs = [
+  {
+    'id': 'Kaytranada',
+    'artist': 'Kaytranada',
+    'year': '2013',
+    'location': 'Montreal',
+    'image': 'Kaytranada.jpg',
+    'reason': 'I picked Kaytranada for two main reasons.\na) It is the first Boiler Room set I ever watched. \nb) it is a ridiculously good set!',
+    'url': 'https://www.youtube.com/watch?v=-5EQIiabJvk'
+  },
+
+  {
+    'id': 'TomTrago',
+    'artist': 'Tom Trago',
+    'year': '2016',
+    'location': 'Amsterdam',
+    'image': 'TomTrago.jpg',
+    'reason': 'This was an obvious choice. \nI love Tom Trago. \nI love boogie/disco. \nSo when the two collide, it is bound to be great!',
+    'url': 'https://www.youtube.com/watch?v=zDp4LLZ0yIk'
+  },
+
+  {
+    'id': 'JoeBataan',
+    'artist': 'Joe Bataan',
+    'year': '2015',
+    'location': 'New York City',
+    'image': 'JoeBataan.jpg',
+    'reason': "I picked Joe Bataan for the sole reason that the man is a legend of Latin Soul! \nI've never had an opportunity to see him in the flesh so this is the next best thing!",
+    'url': 'https://www.youtube.com/watch?v=bAhmoLSqIa8'
+  }
+];
+
+export default djs;
